a61fe3efd179f3d46c3263ae01cd961a5da2f5ae
William Carriello <william.carriello@haymanwoodward.com>
2023-05-31 17:03:03 -0300

    update Codigo Clarity

--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,7 @@ export default function Document() {
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "gn18sylimh");
+            })(window, document, "clarity", "script", "ha5aiozeou");
             `,
           }}
         />
